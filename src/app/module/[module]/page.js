3b4749c0c142f39612a3af61a40b407d64b33508
Martin Marwad <44274319+MartinMarwad@users.noreply.github.com>
2024-03-28 00:38:35 -0400

    Fix

--- src/app/module/[module]/page.js
+++ src/app/module/[module]/page.js
@@ -27,13 +27,13 @@ export default function ModulePage({ params }) {
                         Module Introduction
                     </Typography>
                     <Typography variant="body1" paragraph>
-                        Dive into the fascinating world of robotics programming with our first module, "Robot Dance Basics". This module is designed to introduce beginners to the fundamentals of robotics programming through an engaging project where you'll teach a robot to dance.
+                        Dive into the fascinating world of robotics programming with our first module, {"Robot Dance Basics"}. This module is designed to introduce beginners to the fundamentals of robotics programming through an engaging project where {"you'll"} teach a robot to dance.
                     </Typography>
                     <Typography variant="body1" paragraph>
                         Throughout this module, you will learn about basic concepts of robotics programming, setting up your development environment, the fundamentals of robot movement and control, and finally, choreographing a simple dance routine for a robot.
                     </Typography>
                     <Typography variant="body1" paragraph>
-                        Whether you're new to robotics or looking to refine your programming skills, this module offers a step-by-step guide to get you started on your journey in robotics programming.
+                        Whether {"you're"} new to robotics or looking to refine your programming skills, this module offers a step-by-step guide to get you started on your journey in robotics programming.
                     </Typography>
                     <Stack direction="row" spacing={2} justifyContent="center">
                         <Button variant="contained" color="primary" href="/module/1/1">
